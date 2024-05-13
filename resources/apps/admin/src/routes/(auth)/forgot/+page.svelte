b0c5afadefc822ae116c3dc7b7a309d6d3b45c53
Ramy Merheb <ramymerheb@gmail.com>
2024-05-13 17:20:57 +0300

    =initial

---
+++ resources/apps/admin/src/routes/(auth)/forgot/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	import axios from 'axios';
+
+	let fields = { email: '', password: '' };
+
+	let loading = false;
+
+	const onSubmit = () => {
+		axios
+			.post('/forgot', fields)
+			.then((response) => {
+				goto('/login');
+			})
+			.finally(() => {
+				loading = false;
+			});
+	};
+</script>
+
+<div class="mb-6">
+	<h3 class="h3">Forgot Password</h3>
+	<p>Please provide your email to reset your password.</p>
+</div>
+
+<form action="" on:submit|preventDefault={onSubmit}>
+	<div class="mb-4">
+		<label class="label">
+			<span>Email</span>
+			<input
+				class="input"
+				bind:value={fields.email}
+				name="email"
+				type="email"
+				placeholder="john@doe.com"
+				required
+			/>
+		</label>
+	</div>
+	<button type="submit" class="btn variant-filled-primary w-full font-bold text-white"
+		>Submit</button
+	>
+	<a href="/login" class="block pt-2 text-center">Back to login </a>
+</form>
